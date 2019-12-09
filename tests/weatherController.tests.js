3acf63452184c6f104de8f0b847ab7eb696b46aa
ssantos94 <ssantos@gocontact.pt>
2019-12-09 18:51:33 +0000

    start tests

---
+++ tests/weatherController.tests.js
@@ -0,0 +1,42 @@
+const chai = require("chai");
+const expect = chai.expect;
+const sinon = require("sinon");
+const weatherController = require("../src/resources/weather/weatherController");
+const weatherMethods = require("../src/resources/weather/weatherMethods");
+
+describe("weatherController", function()  {
+  describe("getWeather", function() {
+    it("should return weather for a particular city", async function() {
+
+        const req = {
+            query:{
+                cities:['lisbon']
+            }
+        };
+
+        const res = {
+            send: sinon.spy()
+        };
+        
+        const next = {};
+
+        const stub = {
+            Lisbon: {
+                temp: 16.19,
+                pressure: 1030,
+                humidity: 67,
+                temp_min: 15,
+                temp_max: 17
+            }
+        };
+        
+        const owmGetWeatherReqStub = sinon.stub(weatherMethods, "owmGetWeatherReq")
+            .returns(stub);
+
+        await weatherController.getWeather(req, res, next);
+
+        expect(res.send.firstCall.args[0]).to.eql(stub);
+
+    });
+  });
+});
---
+++ tests/weatherMethods.tests.js
@@ -0,0 +1,74 @@
+const chai = require("chai");
+const expect = chai.expect;
+const sinon = require("sinon");
+const weatherMethods = require("../src/resources/weather/weatherMethods");
+const axios = require('axios');
+
+describe("weatherMethods", function()  {
+    describe("owmGetWeatherReq", async function()  {
+        
+        const cities = ['lisbon'];
+        const apiKey = '123456789';
+
+        const stub = {
+            coord: {
+                lon: -9.14,
+                lat: 38.71
+            },
+            weather: [
+                {
+                    id: 800,
+                    main: "Clear",
+                    description: "clear sky",
+                    icon: "01n"
+                }],
+            base: "stations",
+            main: {
+                temp: 13.43,
+                pressure: 1030,
+                humidity: 82,
+                temp_min: 12.22,
+                temp_max: 15
+            },
+            visibility: 10000,
+            wind: {
+                speed: 3.1,
+                deg: 300
+            },
+            clouds: {
+                all: 2
+            },
+            dt: 1575914678,
+            sys: {
+                type: 1,
+                id: 6901,
+                country: "PT",
+                sunrise: 1575877363,
+                sunset: 1575911704
+            },
+            timezone: 0,
+            id: 2267057,
+            name: "Lisbon",
+            cod: 200
+        }
+
+        const stub2 = {
+            Lisbon: {
+                temp: 16.19,
+                pressure: 1030,
+                humidity: 67,
+                temp_min: 15,
+                temp_max: 17
+            }
+        }
+        
+        const axiosStub = sinon.stub(axios, 'get').returns(stub);
+        const parseRespondeStub = sinon.stub(weatherMethods, 'parseResponse')
+            
+
+        const result = await weatherMethods.owmGetWeatherReq(cities, apiKey);
+
+        expect(result.to.eql(stub));
+
+    });
+});
