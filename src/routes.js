e88f76e660be6d77a4c9e16c1e62608130ea3106
ananas99 <s94santos@gmail.com>
2020-02-03 21:19:10 +0000

    refactor folder structure

--- src/routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router();
-
-const weatherController = require('./resources/weather/weatherController');
-
-router.get('/weather', weatherController.getWeather);
-
-module.exports = router;
